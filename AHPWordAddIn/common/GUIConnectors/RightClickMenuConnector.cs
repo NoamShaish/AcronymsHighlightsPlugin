52f95b3a7c97561b9227a6bed48dd7643598aa52
Alexander Tilkin <alextilk@gmail.com>
2013-04-29 22:04:52 +0300

    Fixed Compilation Error
    o Fixed compilation Error
    o CreatorFactory -
    Changed from public abstract static class CreatorFactory
    To public abstract class CreatorFactory
    Reason abstract class cannot be static

--- AHPWordAddIn/common/GUIConnectors/RightClickMenuConnector.cs
+++ AHPWordAddIn/common/GUIConnectors/RightClickMenuConnector.cs
@@ -40,15 +40,15 @@ namespace AHPWordAddIn.common.GUIConnectors
         {
             CommandBar commandBar = null;
             getCommandBarInstanceByName(ThisAddIn.application.CommandBars, "Text", out commandBar);
-            CleanCommandBarByList(ref commandBar);
-            updateCommandBarByAccronym(e.acronym, ref  commandBar);
+            CleanCommandBar(ref commandBar);
+            updateCommandBarByAccronym(e.acronym, ref commandBar);
         }
 
         /// <summary>
         /// Resets the command bar from previouse results
         /// </summary>
         /// <param name="commandBar">CommandBar instance</param>
-        private void CleanCommandBarByList(ref CommandBar commandBar)
+        private void CleanCommandBar(ref CommandBar commandBar)
         {
             commandBar.Reset();
         }
